d18263f65cb2c2331b190a8a2a15f4c9e12293dd
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-30 14:00:16 +0300

    design the column  for the questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -11,8 +11,18 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return Container(
-      child: Text("HELLO Q's"),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text("Questions"),
+          SizedBox(height: 30),
+          ElevatedButton(onPressed: null, child: Text("Answer 1")),
+          ElevatedButton(onPressed: null, child: Text("Answer 2")),
+          ElevatedButton(onPressed: null, child: Text("Answer 3")),
+        ],
+      ),
     );
   }
 }
