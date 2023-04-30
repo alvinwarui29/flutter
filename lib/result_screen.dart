0980ad16e7a5912637e0368b1963697c9daac567
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-30 19:16:50 +0300

    add result screen

---
+++ lib/result_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+    );
+  }
+}
