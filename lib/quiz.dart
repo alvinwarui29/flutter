f8f7abb09308c1ecbd5f8a2c51f99e6f50c54f47
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-30 12:57:28 +0300

    create two widgets quiz and quiz Q's

---
+++ lib/quiz.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:start/questions_screen.dart';
+import 'start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.lightBlue, Colors.blueGrey],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight)),
+            child: activeScreen),
+      ),
+    );
+  }
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+  @override
+  Widget build(context) {
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Image(
+          image: AssetImage('assets/quiz-logo.png'),
+          color: Color.fromARGB(150, 250, 255, 255),
+        ),
+        const SizedBox(
+          height: 90,
+        ),
+        const Text(
+          'Learn flutter the fun way',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        OutlinedButton.icon(
+            onPressed: null,
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: const Icon(
+              Icons.arrow_right_alt,
+              color: Colors.white,
+            ),
+            label: const Text(
+              'Start quiz',
+              style: TextStyle(color: Colors.white),
+            ))
+      ],
+    ));
+  }
+}
